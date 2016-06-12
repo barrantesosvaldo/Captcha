9751236b024ecd9ff322e51c83188b68fa8e226d
Gabriel Rodriguez <gabrielo94@hotmail.com>
2016-06-11 23:11:06 -0600

    comit perceptron

---
+++ Perceptron.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace IA_Proyecto_III
+{//perceptron = new Perceptron(trainingValues[0].length, hiddenLayerSize, desiredOutput[0].length, learningRate, variationRate, maxPeriods, maxError);
+    class Perceptron
+    {
+        
+        private ProcessUnit[] processUnitsOutput;// PROCESSING UNITS OUTPUT LAYER
+	    private ProcessUnit[] processUnitHidden;// PROCESSING UNITS HIDDEN LAYER
+	    private int inputLayerSize;// NUMBER OF INPUTS
+	    private int hiddenLayerSize;// NUMBER PROCESSING UNITS IN HIDDEN LAYER
+	    private int outputLayerSize;// NUMBER PROCESSING UNITS IN OUTPUT LAYER
+	    private List<Double> errors;
+	    private int periods;
+	    private int maxPeriods;
+	    private double maxError;
+	    private double variationRate;
+	    private double learningRate;
+
+	    public double getVariationRate() {
+		    return variationRate;
+	    }
+
+	    public double getLearningRate() {
+		    return learningRate;
+	    }
+
+	    public int getInputLayerSize() {
+		    return inputLayerSize;
+	    }
+
+	    public int getHiddenLayerSize() {
+		    return hiddenLayerSize;
+	    }
+
+	    public int getOutputLayerSize() {
+		    return outputLayerSize;
+	    }
+
+	    public double getMaxError() {
+		    return maxError;
+	    }
+
+	    public List<Double> getErrors() {
+		    return errors;
+	    }
+
+	    public int getPeriods() {
+		    return periods;
+	    }
+
+	    public int getMaxPeriods() {
+		    return maxPeriods;
+	    }
+
+	    public Perceptron(int inputLayerSize, int hiddenLayerSize, int outputLayerSize, double learningRate, double variationRate, int maxPeriods, double maxError) {
+		    this.inputLayerSize = inputLayerSize;
+		    this.hiddenLayerSize = hiddenLayerSize;
+		    this.outputLayerSize = outputLayerSize;
+		    this.maxPeriods = maxPeriods;
+		    this.variationRate = variationRate;
+		    this.learningRate = learningRate;
+		    this.maxError = maxError;
+
+		    // INIT HIDDEN LAYER
+		    processUnitHidden = new ProcessUnit[hiddenLayerSize];
+
+		    for (int i = 0; i < processUnitHidden.Length; i++) {
+			    processUnitHidden[i] = new ProcessUnit(learningRate, variationRate);
+			    processUnitHidden[i].initWeights(inputLayerSize);
+		    }
+
+		    // INIT OUTPUT LAYER
+
+		    processUnitsOutput = new ProcessUnit[outputLayerSize];
+
+		    for (int i = 0; i < processUnitsOutput.Length; i++) {
+			    processUnitsOutput[i] = new ProcessUnit(learningRate, variationRate);
+			    processUnitsOutput[i].initWeights(hiddenLayerSize);
+		    }
+	    }
+
+	    public double[] output(double[] input) {
+		    // CALCULATE OUTPUT LAYER HIDDEN
+		    double[] V = new double[hiddenLayerSize];
+		    for (int i = 0; i < processUnitHidden.Length; i++) {
+			    double activation = processUnitHidden[i].activation(input);
+			    V[i] = processUnitHidden[i].output(activation);
+		    }
+
+		    // CALCULATE OUTPUT
+		    double[] O = new double[outputLayerSize];
+		    for (int i = 0; i < processUnitsOutput.Length; i++) {
+			    double activation = processUnitsOutput[i].activation(V);
+			    O[i] = processUnitsOutput[i].output(activation);
+		    }
+
+		    return O;
+	    }
+
+	    public double calculateError(double[][] desiredOutput, double[][] inputs) {
+		    double error = 0;
+		    for (int i = 0; i < inputs.Length; i++) {
+
+			    double[] thisoutput = output(inputs[i]);
+
+			    for (int j = 0; j < thisoutput.Length; j++) {
+				    error += Math.Pow(desiredOutput[i][j] - thisoutput[j], 2) / 2;
+			    }
+
+		    }
+
+		    return error;
+	    }
+
+	    public int training(double[][] trainingValues, double[][] desiredOutput) {
+            
+		    periods = 0;
+		    errors = new List<Double>();
+		    double error = 0;
+		    do {
+
+			    double[] activationHiddenLayer = new double[hiddenLayerSize];
+			    double[] activationOutputLayer = new double[outputLayerSize];
+
+			    double[] deltaHiddenLayer = new double[hiddenLayerSize];
+			    double[] deltaOutputLayer = new double[outputLayerSize];
+
+			    double[] V = new double[hiddenLayerSize];
+			    double[] O = new double[outputLayerSize];
+
+			    for (int e = 0; e < trainingValues.Length; e++) {
+
+				    // CALCULATE OUTPUT FORWARD
+				    for (int i = 0; i < processUnitHidden.Length; i++) {
+					    activationHiddenLayer[i] = processUnitHidden[i].activation(trainingValues[e]);
+					    V[i] = processUnitHidden[i].output(activationHiddenLayer[i]);
+				    }
+
+				    for (int i = 0; i < processUnitsOutput.Length; i++) {
+					    activationOutputLayer[i] = processUnitsOutput[i].activation(V);
+					    O[i] = processUnitsOutput[i].output(activationOutputLayer[i]);
+				    }
+
+				    // UPDATE WEIGHTS BACKWARDS
+
+				    for (int i = 0; i < processUnitsOutput.Length; i++) {
+					    deltaOutputLayer[i] = processUnitsOutput[i].delta(desiredOutput[e][i], O[i]);
+					    processUnitsOutput[i].updateWeights(V, deltaOutputLayer[i], activationOutputLayer[i]);
+				    }
+
+				    for (int i = 0; i < processUnitHidden.Length; i++) {
+
+					    double[] W = new double[outputLayerSize];
+
+					    for (int j = 0; j < processUnitsOutput.Length; j++) {
+						    W[j] = processUnitsOutput[j].getWeights()[i];
+					    }
+
+					    deltaHiddenLayer[i] = processUnitHidden[i].delta(deltaOutputLayer, W);
+					    processUnitHidden[i].updateWeights(trainingValues[e], deltaHiddenLayer[i], activationHiddenLayer[i]);
+				    }
+			    }
+			    periods++;
+
+			    // CCALCULATE ERROR
+			    error = calculateError(desiredOutput, trainingValues);
+			    errors.Add(error);
+		    } while (error > maxError && periods < maxPeriods);
+		    return periods;
+	    }
+        }
+    }
+
+
---
+++ ProcessUnit.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_Proyecto_III
+{
+    class ProcessUnit
+    {
+        private double variationRate;
+        private double learningRate;
+        private double[] weights;
+        private double[] previousVariation;
+
+        public ProcessUnit(double learningRate, double variationRate)
+        {
+            this.variationRate = learningRate;
+            this.learningRate = variationRate;
+        }
+
+        public double getLearningRate()
+        {
+            return variationRate;
+        }
+
+        public void setLearningRate(double learningRate)
+        {
+            this.variationRate = learningRate;
+        }
+
+        public double getVariationRate()
+        {
+            return learningRate;
+        }
+
+        public void setVariationRate(double variationRate)
+        {
+            this.learningRate = variationRate;
+        }
+
+        public double[] getWeights()
+        {
+            return weights;
+        }
+
+        public void setWeights(double[] weights)
+        {
+            this.weights = weights;
+        }
+
+        public void initWeights(int n)
+        {
+            weights = new double[n];
+            previousVariation = new double[n];
+            for (int i = 0; i < weights.Length; i++)
+            {
+                Random rnd = new Random();
+                weights[i] = rnd.Next(weights.Length);
+                previousVariation[i] = 0;
+            }
+        }
+
+        public double activation(double[] inputs)
+        {
+            double activation = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                activation += weights[i] * inputs[i];
+            }
+            return activation;
+        }
+
+        public double output(double activation)
+        {
+            return activationFunction(activation);
+        }
+
+        public double activationFunction(double x)
+        {
+            return sigmoid(x);
+        }
+
+        public double activationFunctionDerivative(double x)
+        {
+            return sigmoidDerivative(x);
+        }
+
+        public double delta(double desiredOutput, double output)
+        {
+            return (desiredOutput - output);
+        }
+
+        public double delta(double[] d, double[] W)
+        {
+            double totalDelta = 0;
+            for (int i = 0; i < d.Length; i++)
+            {
+                totalDelta += W[i] * d[i];
+            }
+            return totalDelta;
+        }
+
+        public void updateWeights(double[] inputs, double delta, double activation)
+        {
+            double variation;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                variation = (learningRate * inputs[i] * delta * activationFunctionDerivative(activation)) + previousVariation[i] * variationRate;
+                previousVariation[i] = variation;
+                weights[i] += variation;
+            }
+        }
+
+        public double sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+
+        public double sigmoidDerivative(double x)
+        {
+            return sigmoid(x) * (1 - sigmoid(x));
+        }
+    
+    }
+}
